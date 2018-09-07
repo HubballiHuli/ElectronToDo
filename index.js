f6dd05dd862834a11cf795ef2130fa54b9e08967
HubballiHuli <sachin7034@gmail.com>
2018-09-07 16:53:41 +0530

    Added custom menu

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const electron = require('electron');
 
-const {app,BrowserWindow} = electron;
+const {app,BrowserWindow,Menu} = electron;
 
 let appWindow = null;
 
 function createAppWindow(){
     appWindow = new BrowserWindow({});
     appWindow.loadFile('index.html');
+    mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
+    appWindow.setMenu(mainMenu)
 }
 
+
+let mainMenuTemplate = [
+    {
+        label:"ToDo",
+        submenu:[
+            {
+                label:"Add ToDo",
+            },
+            {
+                label:"Remove ToDo"
+            }
+        ],
+    }
+];
 app.on('ready',createAppWindow);
