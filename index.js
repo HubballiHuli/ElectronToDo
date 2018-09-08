fbd9db6b2186da32221e878a152b122b36bbc9f3
HubballiHuli <sachin7034@gmail.com>
2018-09-08 12:58:11 +0530

    creating new window for adding a new todo

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const electron = require('electron');
 const {app,BrowserWindow,Menu} = electron;
 
 let appWindow = null;
-
+let addNewTodoWindow = null;
 function createAppWindow(){
     appWindow = new BrowserWindow({});
     appWindow.loadFile('index.html');
     mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
     appWindow.setMenu(mainMenu)
+}
+
+//Function to create a new window to add new todo
+function createAddNewToDoWindow(){
+    addNewTodoWindow = new BrowserWindow({height:200,width:300});
+    addNewTodoWindow.loadFile('addNewToDo.html');
+    
 }
 
 
@@ -18,6 +25,10 @@ let mainMenuTemplate = [
         submenu:[
             {
                 label:"Add ToDo",
+                accelerator:(process.platform == 'darwin') ? "Command+Shift+A" : "Control+Shift+A",
+                click(){
+                    createAddNewToDoWindow();
+                }
             },
             {
                 label:"Remove ToDo"
