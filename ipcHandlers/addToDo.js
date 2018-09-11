8197b46fb273e4b1072deb977d8f5dcb7493d4a4
HubballiHuli <sachin7034@gmail.com>
2018-09-11 15:16:06 +0530

    some minor bug fixes and changes

--- ipcHandlers/addToDo.js
+++ ipcHandlers/addToDo.js
@@ -5,5 +5,5 @@ const globals = require('../globals.js');
 exports.addNewToDo = function(e,data){
     console.log(data);
     globals.appWindow.webContents.send('newToDo',data);
-    // globals.addNewTodoWindow.quit();
+    globals.addNewTodoWindow.close();
 };
--- partials/windows.js
+++ partials/windows.js
@@ -5,5 +5,4 @@ exports.createAddNewToDoWindow = function(){
     globals.addNewTodoWindow = new BrowserWindow({height:200,width:300});
     globals.addNewTodoWindow.loadFile('./views/addNewToDo.html');
     globals.addNewTodoWindow.on('closed',()=>{globals.addNewTodoWindow = null});
-    
 }
