f45dc531e7a5162faefb788895a96580b93278d1
HubballiHuli <sachin7034@gmail.com>
2018-09-11 11:55:59 +0530

    first IPC demo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const electron = require('electron');
 
-const {app,BrowserWindow,Menu} = electron;
+const {app,BrowserWindow,Menu,ipcMain} = electron;
 const menus = require('./partials/menus/menu.js');
 
 let appWindow = null;
@@ -10,5 +10,9 @@ function createAppWindow(){
     mainMenu = Menu.buildFromTemplate(menus.mainMenuTemplate)
     appWindow.setMenu(mainMenu)
 }
+
+ipcMain.on('hi',function(e,msg){
+    console.log(msg);
+})
 
 app.on('ready',createAppWindow);
